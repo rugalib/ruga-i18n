3d584172017ed0035bea94a58c8e6d86cc7cc918
Roland Rusch <roland@rusch.lu>
2022-05-19 10:50:50 +0200

    #1 Initial commit

---
+++ src/autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Translate the string to the current language using the TranslatorInterface instance.
+ *
+ * @param string $message
+ *
+ * @return string
+ * @throws Exception
+ */
+function __(string $message): string
+{
+    return $message;
+//    return \Rrlib\I18n\Facade\Localization::getTranslator()->translate($message);
+}
