6a60c5bb4ff8671e65d98cc0f9d40a9be2600f02
Roland Rusch <roland@rusch.lu>
2023-09-02 23:22:15 +0200

    #3 Change License to AGPL-3.0-only

--- src/autoload.php
+++ src/autoload.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * SPDX-FileCopyrightText: 2023 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+declare(strict_types=1);
 
 /**
  * Translate the string to the current language using the TranslatorInterface instance.
